2c990dc4991884ffe5c156ebf6d0718f2682fa5d
mugaheddev <mugaheddev@gmail.com>
2025-07-29 14:41:37 +0200

    يخىث

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,8 @@ app.post('/api/download', async (req, res) => {
       addHeader: [
         'referer:youtube.com',
         'user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-      ]
+      ],
+      youtubeDl: 'yt-dlp' // استخدم yt-dlp بدل youtube-dl
     });
 
     let downloadUrl = null;
